a8971f8a8b245e8a6a4ff5ccba19e307a0860d06
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-07-05 21:38:15 +0300

    add temperature schemas

---
+++ temperature/schemas.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from city.schemas import CityResponse
+
+
+class TemperatureBase(BaseModel):
+    temperature: float
+    date_time: datetime
+
+    model_config = {"from_attributes": True}
+
+class TemperatureRequest(TemperatureBase):
+    city_id: int
+
+class TemperatureResponse(TemperatureBase):
+    id: int
+    city_id: int
+
+class TemperatureDetailResponse(TemperatureResponse):
+    id: int
+    city: CityResponse
