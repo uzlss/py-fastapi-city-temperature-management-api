540f8eb7dd43625a71276f6af3504359a983c033
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-07-05 19:08:08 +0300

    add city schemas

---
+++ city/schemas.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class CityBase(BaseModel):
+    name: str
+    additional_info: Optional[str] = None
+
+    model_config = {"from_attributes": True}
+
+class CityRequest(CityBase):
+    pass
+
+class CityResponse(CityBase):
+    id: int
